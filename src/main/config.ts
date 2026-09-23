a96d338337bad8a459e5efc96b2c4e9cbdeadad9
Romain <78274209+Brize-Glace@users.noreply.github.com>
2026-09-23 21:15:07 +0200

    feat: modify ip adresses for temporary local dev environnement

--- src/main/config.ts
+++ src/main/config.ts
@@ -1,3 +1,3 @@
 declare const __API_URL__: string;
 
-export const API_URL = __API_URL__;
+export const API_URL = __API_URL__.replace(/\/+$/, '');
--- vite.main.config.ts
+++ vite.main.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config
 export default defineConfig({
   define: {
-    __API_URL__: JSON.stringify(process.env.CTRLALTBRO_API_URL ?? 'http://localhost:5173'),
+    __API_URL__: JSON.stringify(process.env.CTRLALTBRO_API_URL ?? 'http://192.168.1.12:5173'),
   },
 });
